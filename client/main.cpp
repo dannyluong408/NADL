e7ee617e762a1187b6d81e8c87e16f7022388bf5
Danny Luong <dannyluong408@gmail.com>
2017-03-08 09:24:40 -0800

    Update main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -5,20 +5,12 @@
 
 MainWindow *w;
 
-
-
-
-
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-
-
+    
     w = new MainWindow;
-
     w->setWindowIcon(QIcon(":/oldguy.ico"));
-
     w->show();
-
     return a.exec();
 }
 
